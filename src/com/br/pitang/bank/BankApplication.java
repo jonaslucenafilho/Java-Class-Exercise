42353a54a116a34f3ab1339c0c1b0505e9e14cdd
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-10 15:03:25 -0300

    Created two account type objects, assigning balance values ​​to each one

--- src/com/br/pitang/bank/BankApplication.java
+++ src/com/br/pitang/bank/BankApplication.java
@@ -1,10 +1,19 @@
 package com.br.pitang.bank;
 
+import com.br.pitang.bank.model.Account;
+import com.br.pitang.bank.model.Client;
+
 public class BankApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Client c1 = new Client();
+		
+		Account a1 = new Account();
+		Account a2 = new Account();
+		
+		a1.setBalance(100);
+		a2.setBalance(0);
 	}
 
 }
