2d7f3c1f2b2296c0d283f4a97de8a1b93951c562
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-10 14:41:56 -0300

    Added attributes, constructor and methods getters and setters

--- src/com/br/pitang/bank/model/Client.java
+++ src/com/br/pitang/bank/model/Client.java
@@ -1,5 +1,29 @@
 package com.br.pitang.bank.model;
 
 public class Client {
+	
+	private String name;
+	private String cpf;
+	
+	public Client (String name, String cpf ) {
+		this.name = name;
+		this.cpf = cpf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	private void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
 }
