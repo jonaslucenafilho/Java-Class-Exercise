d81dee2152b926f66f8d374ef363e95dbca2e794
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-10 15:08:55 -0300

    Testing debit and credit methods in main

--- src/com/br/pitang/bank/BankApplication.java
+++ src/com/br/pitang/bank/BankApplication.java
@@ -14,6 +14,24 @@ public class BankApplication {
 		
 		a1.setBalance(100);
 		a2.setBalance(0);
+		
+		System.out.println("O valor do saldo da conta a1 é de R$ " + a1.getBalance());
+		System.out.println("O valor do saldo da conta a2 é de R$ " + a2.getBalance());
+		System.out.println();
+		
+		a1.credit(200.50);
+		a2.credit(1000);
+		
+		System.out.println("O valor do saldo da conta a1 é de R$ " + a1.getBalance());
+		System.out.println("O valor do saldo da conta a2 é de R$ " + a2.getBalance());
+		System.out.println();
+		
+		a1.debit(100);
+		a2.debit(300);
+		
+		System.out.println("O valor do saldo da conta a1 é de R$ " + a1.getBalance());
+		System.out.println("O valor do saldo da conta a2 é de R$ " + a2.getBalance());
+		System.out.println();
 	}
 
 }
