d825f1d1fc633fc7296f1b03907d69bdb6b6ec2e
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-10 14:49:44 -0300

    Added attributes, constructor and methods getters and setters in Account Class

--- src/com/br/pitang/bank/model/Account.java
+++ src/com/br/pitang/bank/model/Account.java
@@ -2,4 +2,44 @@ package com.br.pitang.bank.model;
 
 public class Account {
 
+	private String number;
+	private double balance;
+	private Client owner;
+	
+	public Account (String number, Client owner) {
+		this.number = number;
+		this.owner = owner;
+	}
+
+	public Account(String number, double balance, Client owner) {
+		this.number = number;
+		this.balance = balance;
+		this.owner = owner;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	private void setNumber(String number) {
+		this.number = number;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	private void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Client getOwner() {
+		return owner;
+	}
+
+	private void setOwner(Client owner) {
+		this.owner = owner;
+	}
+	
+	
 }
