627ce7b87a6e53f492e8a0716777600a645bd7db
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-10 15:48:22 -0300

    Create new objects in main

--- src/com/br/pitang/bank/BankApplication.java
+++ src/com/br/pitang/bank/BankApplication.java
@@ -8,11 +8,21 @@ public class BankApplication {
 	public static void main(String[] args) {
 		
 		Client c1 = new Client();
+		c1.setCpf("05190955451");
+		c1.setName("Jonas Lucena");
+		
+		Client c2 = new Client();
+		c2.setCpf("06091984403");
+		c2.setName("Marina Andrade");
 		
 		Account a1 = new Account();
-		Account a2 = new Account();
+		a1.setNumber("1509-1");
+		a1.setOwner(c1);
+		a1.setBalance(0);
 		
-		a1.setBalance(100);
+		Account a2 = new Account();
+		a2.setNumber("1007-3");
+		a2.setOwner(c2);
 		a2.setBalance(0);
 		
 		System.out.println("O valor do saldo da conta a1 é de R$ " + a1.getBalance());
